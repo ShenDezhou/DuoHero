66c538fb2d0fe5a177483689f6bbc5fd6cd9489d
shen <shen@yhwdeMacBook-Air.local>
2016-11-09 22:29:42 +0800

    snow scene

---
+++ Assets/storm.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var wz:WindZone;
+
+function Start () {
+	wz = GetComponent.<WindZone>();
+		
+}
+
+function Update () {
+//	transform.Rotate(Vector3(0,90,0)*Time.deltaTime);
+	wz.windMain=Random.Range(0.0f, 1.0f);
+
+
+}
