fafaa1873b4386904780124c08795a73925aceeb
shen <shen@yhwdeMacBook-Air.local>
2016-11-26 09:21:21 +0800

    stage.json using json format

--- Assets/Scripts/GM_EnemySpawner.cs
+++ Assets/Scripts/GM_EnemySpawner.cs
@@ -1,140 +1,136 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using SimpleJSON;
-//-----------------------Serializable Classes-----------------------
+
+public class SpawnType
+{
+	public string tag;
+	public Vector3 initPosition;
+	public float speed;
+	public Queue<int> enemyInterval;
+
+	public SpawnType()
+	{
+		enemyInterval = new Queue<int> ();
+	}
+}
+
 [System.Serializable]
 public class EnemyType
 {
-    public GameObject[] enemyPrefab;
-	public float intervalBetweenWaves;
+	[HideInInspector]
 	public bool isFromLeft;
-
 	[HideInInspector]
 	public int spawned;
-	[HideInInspector]
-	public bool startTrigger;
 
-	private int[] enemiesMax = new int[5];
+	SpawnType[] enemyKind = new SpawnType[5];
+	int[] enemyQueInt = new int[5];
 
-	float intervalBetweenSpawns;
-	int randNum;
-	// read config file for enemy spawn interval.
-	JSONNode enemy_json;
-
-	public void Run(GM_EnemySpawner es, int a, int b, int c, int d, int e)
-    {
-		enemiesMax [0] = a;
-		enemiesMax [1] = b;
-		enemiesMax [2] = c;
-		enemiesMax [3] = d;
-		enemiesMax [4] = e;
-        es.StartCoroutine(Spawn());
-    }
-
-	public void Run(GM_EnemySpawner es, JSONNode enemies_level)
+	public void Run(ref Queue<SpawnType> enemyQueue)
 	{
-		for (int i = 0; i < enemies_level.Count; i++) {
-			enemiesMax [i] = Random.Range (enemies_level [i] ["amount"] [0].AsInt, enemies_level [i] ["amount"] [1].AsInt);
+		if ((enemyQueue.Count >= 5) && (spawned == 0)) {
+			for (int i = 0; i < 5; i++) {
+				enemyKind [i] = enemyQueue.Dequeue ();
+				enemyQueInt [i] = 0;
+				spawned += enemyKind [i].enemyInterval.Count;
+			}
 		}
-		es.StartCoroutine(Spawn());
+
+		for (int i = 0; i < 5; i++) {
+			while ((enemyKind [i].enemyInterval.Count > 0) && (enemyKind [i].enemyInterval.Peek () <= enemyQueInt[i]))
+			{
+				enemyKind [i].enemyInterval.Dequeue ();
+				enemyQueInt [i] = 0;
+				GameObject en = GM_ObjectPool.instance.GetPoolObject (enemyKind[i].tag);
+				if (en == null) {
+					Debug.Log ("enemy null");
+				}
+
+				if (!isFromLeft) {
+					en.transform.position = new Vector3(-1 * en.transform.position.x, en.transform.position.y, en.transform.position.z);
+					en.transform.rotation = Quaternion.Euler(en.transform.rotation.x, 180, en.transform.position.z);
+				}
+				en.GetComponent<Enemy> ().speed = enemyKind [i].speed;
+				en.SetActive (true);
+			}
+
+			enemyQueInt[i]++;
+		}
 	}
-
-    IEnumerator Spawn()
-    {
-		if (enemy_json==null)
-			enemy_json = JSONData.LoadFromFile (Application.streamingAssetsPath + "/duohero.cfg");
-		
-		yield return new WaitForSeconds(intervalBetweenWaves);
-
-		while((enemiesMax[0] > 0) || (enemiesMax[1] > 0) || (enemiesMax[2] > 0) || (enemiesMax[3] > 0) || (enemiesMax[4] > 0))
-        {
-			randNum = Random.Range (0, 5);
-			if (enemiesMax [randNum] <= 0)
-				continue;
-
-			enemiesMax [randNum]--;
-
-			GameObject en = GM_ObjectPool.instance.GetPoolObject(enemyPrefab[randNum].tag);
-			if (en == null) {
-				yield return null;
-			}
-
-			if (!isFromLeft) {
-				en.transform.position = new Vector3(-1 * en.transform.position.x, en.transform.position.y, en.transform.position.z);
-				en.transform.rotation = Quaternion.Euler(en.transform.rotation.x, 180, en.transform.position.z);
-			}
-
-			spawned++;
-            en.SetActive(true);
-
-			//-----------------------Settings-----------------------
-			JSONNode node =  enemy_json [enemyPrefab [randNum].name ];
-			intervalBetweenSpawns = Random.Range (node["interval"] [0].AsFloat, node["interval"] [1].AsFloat) * node["interval_ratio"].AsFloat;
-//			switch (enemyPrefab[randNum].name)
-//			{
-//				case "Enemy_Novice": intervalBetweenSpawns = Random.Range(1, 6) * 0.5f; break;
-//				case "Enemy_BigGuy": intervalBetweenSpawns = Random.Range(3, 5) * 1f; break;
-//				case "Enemy_Archer": intervalBetweenSpawns = Random.Range(4, 7) * 1f; break;
-//				case "Enemy_Rider" : intervalBetweenSpawns = Random.Range(15, 20) * 1f; break;
-//				case "Enemy_FlyMan" : intervalBetweenSpawns = Random.Range(15, 20) * 1f; break;
-//            }
-            yield return new WaitForSeconds(intervalBetweenSpawns);
-        }
-
-		startTrigger = true;
-    }
 }
 
-//---------------------------------------------------------------------
 public class GM_EnemySpawner : MonoBehaviour
 {
+	[HideInInspector]
 	public EnemyType wavesL, wavesR;
-	JSONNode level_json ;
+	FileStream stage;
+	StreamReader sr;
+	Queue<SpawnType> enemyQueue = new Queue<SpawnType>();
+
 	void Awake(){
+//		string temp;
+//		char[] charArray = new char[]{','};
+//		char[] comment = new char[]{'/', '/'};
+
 		wavesL.spawned = 0;
 		wavesR.spawned = 0;
-		wavesL.startTrigger = true;
-		wavesR.startTrigger = true;
+		wavesL.isFromLeft = true;
+		wavesR.isFromLeft = false;
+
+//		if (File.Exists ("Assets/stage.txt")) {
+//			stage = new FileStream ("Assets/stage.txt", FileMode.Open, FileAccess.Read);
+//		} else {
+//			Debug.Log ("file error!");
+//		}
+//
+//		sr = new StreamReader (stage);
+//		while ((temp = sr.ReadLine ()) != null) {
+//			if (temp.StartsWith ("//"))
+//				continue;
+//
+//			int todelete = temp.IndexOfAny (comment);
+//			if(todelete > 0)
+//				temp = temp.Remove (todelete);
+//
+//			string[] strArray = temp.Split (charArray);
+//			SpawnType elem = new SpawnType();
+//
+//			elem.tag = strArray [0];
+//			if (strArray.Length > 5) {
+//				elem.initPosition = Vector3.zero;
+//				elem.speed = System.Convert.ToSingle (strArray [4]);
+//			}
+//
+//			for (int i = 5; i < strArray.Length; i++)
+//				elem.enemyInterval.Enqueue (System.Convert.ToInt32 (strArray [i]));
+//
+//			//			Debug.Log (temp);
+//			enemyQueue.Enqueue (elem);
+//		}
+		// using json
+		JSONNode level_json = JSONData.LoadFromFile (Application.streamingAssetsPath + "/level.cfg");
+		for (int i = 0; i < level_json.Count; i++) {
+			SpawnType elem = new SpawnType ();
+			elem.tag = level_json [i] ["tag"];
+			elem.initPosition = new Vector3(level_json [i] ["pos"][0].AsFloat,level_json [i] ["pos"][1].AsFloat,level_json [i] ["pos"][2].AsFloat);
+			elem.speed = level_json [i] ["speed"].AsFloat;
+			for (int j = 0; j < level_json [i] ["interval"].Count; j++)
+				elem.enemyInterval.Enqueue (level_json [i] ["interval"] [j].AsInt);
+			enemyQueue.Enqueue (elem);
+		}
 
 	}
 
-    void Start()
-    {
-		if(level_json==null)
-			level_json = JSONData.LoadFromFile (Application.streamingAssetsPath + "/level.cfg");
-		
-//		wavesL.Run(this, 5, 0, 0, 0, 0);
-//		wavesR.Run(this, 3, 0, 0, 0, 0);
+	void FixedUpdate()
+	{
+		wavesL.Run (ref enemyQueue);
+		wavesR.Run (ref enemyQueue);
 	}
 
-    void Update()
-    {
-//		#if _A_
-		// to do while K.O. a wave of enemies
-		if ((wavesL.startTrigger))
-		{
-			wavesL.startTrigger = false;			// re-initiallize
-			//next wave
-//			wavesL.Run(this, Random.Range(3, 6), Random.Range(1, 4), Random.Range(0, 3), Random.Range(0, 2), 1);
-			wavesL.Run(this,level_json[wavesL.spawned++]["wavesL"]);
-			//停在最后一关的难度水平
-			if (wavesL.spawned == level_json.Count)
-				wavesL.spawned--;
-		}
-
-		// to do while K.O. a wave of enemies
-		if ((wavesR.startTrigger))
-		{
-			wavesR.startTrigger = false;			// re-initiallize
-
-			//next wave
-//			wavesR.Run(this, Random.Range(4, 7), Random.Range(1, 4), Random.Range(1, 4), 0, 0);
-			wavesR.Run(this,level_json[wavesR.spawned++]["wavesR"]);
-			//停在最后一关的难度水平
-			if (wavesR.spawned == level_json.Count)
-				wavesR.spawned--;
-		}
-//		#endif
+	void OnDestroy()
+	{
+		stage.Close ();
 	}
 }
---
+++ Assets/Scripts/lib/Request.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Request : MonoBehaviour {
+	
+	void Start () {
+		string url = "http://192.168.1.113:8888/rest/duohero/level";
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www));
+	}
+
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null)
+		{
+			Debug.Log("WWW Ok!: " + www.text);
+			System.IO.File.WriteAllText(Application.streamingAssetsPath + "/level.cfg",www.text);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+}
